4ebf8504729e54205742fdcaa8c3762705a8bfb8
AyakaIsWorking <hongara1590@gmail.com>
2024-12-30 09:26:19 +0700

    lan 2

--- main.js
+++ main.js
@@ -1,79 +1,23 @@
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = menuBtn.querySelector("i");
-
-menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
-
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-});
-
-navLinks.addEventListener("click", (e) => {
-  navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-line");
-});
-
-const scrollRevealOption = {
-  origin: "bottom",
-  distance: "50px",
-  duration: 1000,
+window.onscroll = function() {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollToTopBtn.style.display = "block";
+  } else {
+      scrollToTopBtn.style.display = "none";
+  }
 };
 
-ScrollReveal().reveal(".header__image img", {
-  ...scrollRevealOption,
-  origin: "right",
-});
-ScrollReveal().reveal(".header__content h1", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-ScrollReveal().reveal(".header__content p", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-ScrollReveal().reveal(".header__btns", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
+// Khi người dùng nhấp vào nút Scroll to Top
+document.getElementById("scrollToTopBtn").onclick = function() {
+  document.body.scrollTop = 0; // Cho Safari
+  document.documentElement.scrollTop = 0; // Cho Chrome, Firefox, IE và Opera
+};
 
-const banner = document.querySelector(".banner__container");
-
-const bannerContent = Array.from(banner.children);
-
-bannerContent.forEach((item) => {
-  const duplicateNode = item.cloneNode(true);
-  duplicateNode.setAttribute("aria-hidden", true);
-  banner.appendChild(duplicateNode);
-});
-
-ScrollReveal().reveal(".arrival__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
-
-ScrollReveal().reveal(".sale__image img", {
-  ...scrollRevealOption,
-  origin: "left",
-});
-ScrollReveal().reveal(".sale__content h2", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-ScrollReveal().reveal(".sale__content p", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-ScrollReveal().reveal(".sale__content h4", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-ScrollReveal().reveal(".sale__btn", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-
-ScrollReveal().reveal(".favourite__card", {
-  ...scrollRevealOption,
-  interval: 500,
+// Hiển thị thông báo khi nhấp vào một sản phẩm
+const productElements = document.querySelectorAll('.product');
+productElements.forEach(product => {
+  product.addEventListener('click', function() {
+      const productName = this.querySelector('p').textContent; // Lấy tên sản phẩm
+      alert("Bạn đã chọn sản phẩm: " + productName);
+  });
 });
